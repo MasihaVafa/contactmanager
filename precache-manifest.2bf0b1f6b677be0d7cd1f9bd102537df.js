6d3cbed3f6a06398ca5719b97c1cb4ff5ff0b778
unknown <zahedivafa7@gmail.com>
2019-02-17 19:55:30 +0330

    Updates

---
+++ precache-manifest.2bf0b1f6b677be0d7cd1f9bd102537df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "96f2b8ca3d4c46608312",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "96f2b8ca3d4c46608312",
+    "url": "/contactmanager/static/js/main.96f2b8ca.chunk.js"
+  },
+  {
+    "revision": "8c152fa2d43575c35dff",
+    "url": "/contactmanager/static/css/1.22518a91.chunk.css"
+  },
+  {
+    "revision": "8c152fa2d43575c35dff",
+    "url": "/contactmanager/static/js/1.8c152fa2.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "1ac29f20a368958d4f3a90dc13fcb0e3",
+    "url": "/contactmanager/index.html"
+  }
+];
